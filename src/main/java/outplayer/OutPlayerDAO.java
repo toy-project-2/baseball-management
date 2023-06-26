1bfa3d15c2ccd3aeb65c00c428e8df85edd9fc5a
HanHyeJi <hjhan.0621@gmail.com>
2023-06-26 21:45:12 +0900

    Add: OutPlayerDAO 생성

--- src/main/java/outplayer/OutPlayerDAO.java
+++ src/main/java/outplayer/OutPlayerDAO.java
@@ -1,4 +1,12 @@
 package outplayer;
 
+import java.sql.Connection;
+
 public class OutPlayerDAO {
+    private Connection connection;
+
+    public OutPlayerDAO(Connection connection) {
+        this.connection = connection;
+    }
+
 }
