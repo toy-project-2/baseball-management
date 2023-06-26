e4edbe84bd7bc68ab018ed3b109c55ebea6a47ad
HanHyeJi <hjhan.0621@gmail.com>
2023-06-26 21:29:08 +0900

    Add: PlayerDAO 생성

---
+++ src/main/java/outplayer/OutPlayerDAO.java
@@ -0,0 +1,4 @@
+package outplayer;
+
+public class OutPlayerDAO {
+}
---
+++ src/main/java/player/PlayerDAO.java
@@ -0,0 +1,14 @@
+package player;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerDAO {
+    private Connection connection;
+
+    public PlayerDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+}
