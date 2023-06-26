3136a60aea853f16e65ec8ac1700340bfbb27391
HanHyeJi <hjhan.0621@gmail.com>
2023-06-26 21:31:17 +0900

    Add: PlayerDAO 클래스 - createPlayer() 생성

--- src/main/java/player/PlayerDAO.java
+++ src/main/java/player/PlayerDAO.java
@@ -11,4 +11,19 @@ public class PlayerDAO {
         this.connection = connection;
     }
 
+    public void createPlayer (int teamId, String playerName, String position) {
+        String query = "insert into player_tb (team_id, player_name, position, created_at) values(?, ?, ?, now())";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, teamId);
+            statement.setString(2, playerName);
+            statement.setString(3, position);
+
+            int result = statement.executeUpdate();
+            System.out.println("result = " + result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
