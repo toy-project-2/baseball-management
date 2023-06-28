0d0b0c7473cb2c901469e466feb8c241708b620c
HanHyeJi <hjhan.0621@gmail.com>
2023-06-29 06:16:48 +0900

    Add: PlayerDAO 클래스 - playerGroupByPosition() 생성 (포지션별목록 메서드)

--- src/main/java/player/PlayerDAO.java
+++ src/main/java/player/PlayerDAO.java
@@ -1,17 +1,30 @@
 package player;
 
+import dto.PositionRespDTO;
+
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PlayerDAO {
-    private Connection connection;
+    private static Connection connection;
+    private static PlayerDAO playerDAO;
 
-    public PlayerDAO(Connection connection) {
+    private PlayerDAO(Connection connection) {
         this.connection = connection;
     }
 
-    public void createPlayer (int teamId, String playerName, String position) {
+    public static PlayerDAO getInstance(Connection connection) {
+        if (playerDAO == null)
+            playerDAO = new PlayerDAO(connection);
+        return playerDAO;
+    }
+
+    /**
+     * 선수등록 (insert)
+     */
+    public int insertPlayerByQuery (int teamId, String playerName, String position) {
         String query = "insert into player_tb (team_id, player_name, position, created_at) values(?, ?, ?, now())";
         try {
             PreparedStatement statement = connection.prepareStatement(query);
@@ -20,19 +33,25 @@ public class PlayerDAO {
             statement.setString(3, position);
 
             int result = statement.executeUpdate();
-            System.out.println("result = " + result);
+            return result;
         } catch (SQLException e) {
             e.printStackTrace();
+
         }
+        return 0;
     }
 
-    public List<Player> getAllPlayer() {
+    /**
+     * 선수목록 (select)
+     */
+    public List<Player> selectPlayerByQuery(int teamId) {
         List<Player> players = new ArrayList<>();
-        String query = "select * from player_tb";
+        String query = "select * from player_tb where team_id = ?";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement Statement = connection.prepareStatement(query);
+            Statement.setInt(1, teamId);
 
-            ResultSet rs = preparedStatement.executeQuery();
+            ResultSet rs = Statement.executeQuery();
 
             while (rs.next()) {
                 Player player = new Player(
@@ -52,26 +71,65 @@ public class PlayerDAO {
         return players;
     }
 
-    public void deletePlayer(int playerId) {
-        String query = "delete from player_tb where player_id = ?";
-
-        try {
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, playerId);
-            statement.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void emitPlayer(int playerId) {
+    /**
+     * 선수퇴출 (update)
+     */
+    public int emitPlayerByQuery(int playerId) {
         String query = "update player_tb set team_id = ? where player_Id = ?";
 
         try {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setNull(1, Types.INTEGER);
             statement.setInt(2, playerId);
+
+            int result = statement.executeUpdate();
+            return result;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /**
+     * 포지션별목록 (피벗테이블)
+     */
+    public List<PositionRespDTO> playerGroupByPosition() {
+        List<PositionRespDTO> playersGroupByPosition = new ArrayList<>();
+        String query = "SELECT\n" +
+                "  position,\n" +
+                "  MAX(CASE WHEN team_id = '1' THEN player_name END) AS team1,\n" +
+                "  MAX(CASE WHEN team_id = '2' THEN player_name END) AS team2,\n" +
+                "  MAX(CASE WHEN team_id = '3' THEN player_name END) AS team3\n" +
+                "FROM player_tb\n" +
+                "GROUP BY position";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()) {
+                PositionRespDTO positionRespDTO = PositionRespDTO.builder()
+                        .position(rs.getString("position"))
+                        .team1(rs.getString("team1"))
+                        .team2(rs.getString("team2"))
+                        .team3(rs.getString("team3"))
+                        .build();
+                playersGroupByPosition.add(positionRespDTO);
+            }
+            return playersGroupByPosition;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return playersGroupByPosition;
+    }
+
+    public void deletePlayerByQuery(int playerId) {
+        String query = "delete from player_tb where player_id = ?";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, playerId);
             statement.executeUpdate();
 
         } catch (SQLException e) {
